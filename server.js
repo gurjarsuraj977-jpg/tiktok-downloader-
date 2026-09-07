28fbe10bed53076caca5d103a14e15567f78edf6
gurjarsuraj977-jpg <gurjarsuraj977@gmail.com>
2026-09-07 08:40:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const crypto = require("crypto");
-const ytdlp = require("yt-dlp-exec");
+const ytdlp = require("youtube-dl-exec");
 
 const app = express();
 
